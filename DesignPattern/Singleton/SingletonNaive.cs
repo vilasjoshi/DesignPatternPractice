0347ca8be75d6f00be49bb2dcfde7b3a37b33780
vilasjoshi <vilasjoshi222@gmail.com>
2023-05-21 20:25:15 +0530

    Singleton with Naive approach and unit tested the code.

---
+++ DesignPattern/Singleton/SingletonNaive.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace DesignPattern.Singleton
+{
+	public sealed class SingletonNaive
+	{
+		private static SingletonNaive _instance;
+
+		private SingletonNaive()
+		{
+			Console.WriteLine("Constructor Invoked");
+			//Logger.Log("Constructor Invoked...");
+		}
+
+		public static SingletonNaive Instance
+		{
+			get
+			{
+				Console.WriteLine("Instance Called ...");
+				if(_instance == null)
+				{
+					_instance = new SingletonNaive();
+
+				}
+				return _instance;
+			}
+		}
+	}
+}
+
+// this approach is naive and not thread safe, it can have issue when used with parallelism.
---
+++ DesignPatterns.Test/Singleton.Test/SingletonNaiveTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesignPattern.Singleton;
+namespace DesignPatterns.Test.Singleton.Test
+{
+	[TestClass]
+	public class SingletonNaiveTest
+	{
+		public SingletonNaiveTest()
+		{
+		}
+
+		[TestMethod]
+		public void ReturnsNonNullInstance()
+		{
+            var result = SingletonNaive.Instance;
+			Assert.IsNotNull(result);
+			Assert.IsInstanceOfType(result, typeof(SingletonNaive));
+
+		}
+
+		[TestMethod]
+		public void SingleInstanceOnThreeInstanceCall()
+		{
+			var one = SingletonNaive.Instance;
+			var two = SingletonNaive.Instance;
+			var three = SingletonNaive.Instance;
+
+			Assert.AreEqual(one, two);
+			Assert.AreEqual(two, three);
+		}
+
+		[TestMethod]
+		public void CallingOnDifferentParallelThreads_ShouldFail()
+		{
+			var strings = new List<string>() { "one", "two", "three" };
+			var instances = new List<SingletonNaive>();
+			var parallel = new ParallelOptions() { MaxDegreeOfParallelism = 3 };
+			Parallel.ForEach(strings, parallel, instance =>
+			{
+				instances.Add(SingletonNaive.Instance);
+			});
+			Assert.AreNotEqual(instances[0], instances[1]);
+			Assert.AreNotEqual(instances[1], instances[2]);
+			Assert.AreNotEqual(instances[0], instances[2]);
+        }
+	}
+}
+
